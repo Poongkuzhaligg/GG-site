2f00f7365c5e7b98b32a5d61fcade6ebf9d72ea7
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-06-30 17:22:15 +0530

    card

---
+++ Memory Matching Game/MemMat.js
@@ -0,0 +1,7 @@
+var x = document.querySelectorAll("box-img");
+x.addEventListener('click', function() {
+    x.classList.toggle('is-flipped');
+});
+// function flipTheImage() {
+//     document.getElementsByClassName("flip-img").classList.toggle("is-flipped");
+//   }
