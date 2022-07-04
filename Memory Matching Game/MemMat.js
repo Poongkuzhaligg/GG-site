74d216f7383288bcfcd0c33251dae0948c4be154
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-07-04 23:31:08 +0530

    commit4/7/2

--- Memory Matching Game/MemMat.js
+++ Memory Matching Game/MemMat.js
@@ -1,7 +1,48 @@
-var x = document.querySelectorAll("box-img");
-x.addEventListener('click', function() {
-    x.classList.toggle('is-flipped');
-});
-// function flipTheImage() {
-//     document.getElementsByClassName("flip-img").classList.toggle("is-flipped");
-//   }
+function image1() {
+    document.getElementById("img-1").src="./candy.png";
+}
+function image2() {
+    document.getElementById("img-2").src="./cottoncandy.png";
+}
+function image3() {
+    document.getElementById("img-3").src="./lolipop.png";
+}
+function image4() {
+    document.getElementById("img-4").src="./cane.png";
+}
+function image5() {
+    document.getElementById("img-5").src="./jelly.png";
+}
+function image6() {
+    document.getElementById("img-6").src="./corn.png";
+}
+function image7() {
+    document.getElementById("img-7").src="./candy.png";
+}
+function image8() {
+    document.getElementById("img-8").src="./choco.webp";
+}
+function image9() {
+    document.getElementById("img-9").src="./heartcandy.png";
+}
+function image10() {
+    document.getElementById("img-10").src="./lolipop.png";
+}
+function image11() {
+    document.getElementById("img-11").src="./jelly.png";
+}
+function image12() {
+    document.getElementById("img-12").src="./cane.png";
+}
+function image13() {
+    document.getElementById("img-13").src="./cottoncandy.png";
+}
+function image14() {
+    document.getElementById("img-14").src="./corn.png";
+}
+function image15() {
+    document.getElementById("img-15").src="./choco.webp";
+}
+function image16() {
+    document.getElementById("img-16").src="./heartcandy.png";
+}
