4a1e231d8b0400ff56e18060e7ef73bc23a34c01
gpoongkuzhali <gpoongkuzhali@greatergoods.com>
2022-07-11 09:34:49 +0530

    commitjs

--- Memory Matching Game/MemMat.js
+++ Memory Matching Game/MemMat.js
@@ -1,48 +1,53 @@
+
+const items = [ [{x1}, {x2}, {x3}, {x4}],
+[{x5}, {x6}, {x7}, {x8}], [{x9}, {x10}, {x11}, {x12}], [{x13}, {x14}, {x15}, {x16}] ];
+
 function image1() {
-    document.getElementById("img-1").src="./candy.png";
+    var x1 = document.getElementById("img-1").src="./candy.png";
 }
 function image2() {
-    document.getElementById("img-2").src="./cottoncandy.png";
+    var x2 = document.getElementById("img-2").src="./cottoncandy.png";
 }
 function image3() {
-    document.getElementById("img-3").src="./lolipop.png";
+    var x3 = document.getElementById("img-3").src="./lolipop.png";
 }
 function image4() {
-    document.getElementById("img-4").src="./cane.png";
+    var x4 = document.getElementById("img-4").src="./cane.png";
 }
 function image5() {
-    document.getElementById("img-5").src="./jelly.png";
+    var x5 = document.getElementById("img-5").src="./jelly.png";
 }
 function image6() {
-    document.getElementById("img-6").src="./corn.png";
+    var x6 = document.getElementById("img-6").src="./corn.png";
 }
 function image7() {
-    document.getElementById("img-7").src="./candy.png";
+    var x7 = document.getElementById("img-7").src="./candy.png";
 }
 function image8() {
-    document.getElementById("img-8").src="./choco.webp";
+    var x8 = document.getElementById("img-8").src="./choco.webp";
 }
 function image9() {
-    document.getElementById("img-9").src="./heartcandy.png";
+    var x9 = document.getElementById("img-9").src="./heartcandy.png";
 }
 function image10() {
-    document.getElementById("img-10").src="./lolipop.png";
+    var x10 = document.getElementById("img-10").src="./lolipop.png";
 }
 function image11() {
-    document.getElementById("img-11").src="./jelly.png";
+    var x11 = document.getElementById("img-11").src="./jelly.png";
 }
 function image12() {
-    document.getElementById("img-12").src="./cane.png";
+    var x12 = document.getElementById("img-12").src="./cane.png";
 }
 function image13() {
-    document.getElementById("img-13").src="./cottoncandy.png";
+    var x13 = document.getElementById("img-13").src="./cottoncandy.png";
 }
 function image14() {
-    document.getElementById("img-14").src="./corn.png";
+    var x14 = document.getElementById("img-14").src="./corn.png";
 }
 function image15() {
-    document.getElementById("img-15").src="./choco.webp";
+    var x15 = document.getElementById("img-15").src="./choco.webp";
 }
 function image16() {
-    document.getElementById("img-16").src="./heartcandy.png";
+    var x16 = document.getElementById("img-16").src="./heartcandy.png";
 }
+
